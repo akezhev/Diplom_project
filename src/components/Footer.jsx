3d21f96b97637dddb35f0a02f5e905d5617c13de
Tim <151851166+akezhev@users.noreply.github.com>
2024-09-11 17:17:18 +0300

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="banner_footer"></div>
+      <p>Все права защищены &copy;</p>
+    </footer>
+  );
+}
