fc79c77a523647a505be5c6839b9c2d3819e4a3f
Tim <151851166+akezhev@users.noreply.github.com>
2024-09-11 17:17:55 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,199 @@
-import { useState } from "react";
-import headerLogo from "../public/images/oi_cho.png";
-import Header from "./Components/Header/Header.jsx";
-import Footer from "./Components/Footer/Footer.jsx";
-import "./App.css";
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
+import Categories from "./components/Categories";
+import ShowFullItem from "./components/ShowFullItem";
 
-function App() {
-  const [count, setCount] = useState(0);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      orders: [],
+      currentItems: [],
+      items: [
+        {
+          id: 1,
+          title: "белая",
+          img: "white_1.jpeg",
+          desc: "lorem ipsum",
+          category: "white",
+          price: "100.99",
+        },
+        {
+          id: 2,
+          title: "черная",
+          img: "black_1.jpeg",
+          desc: "lorem ipsum",
+          category: "black",
+          price: "89.99",
+        },
+        {
+          id: 3,
+          title: "белая",
+          img: "white_2.jpeg",
+          desc: "lorem ipsum",
+          category: "white",
+          price: "79.99",
+        },
+        {
+          id: 4,
+          title: "черная",
+          img: "black_2.jpeg",
+          desc: "lorem ipsum",
+          category: "black",
+          price: "109.99",
+        },
+        {
+          id: 5,
+          title: "белая",
+          img: "white_3.jpeg",
+          desc: "lorem ipsum",
+          category: "white",
+          price: "99",
+        },
+        {
+          id: 6,
+          title: "черная",
+          img: "black_3.jpeg",
+          desc: "lorem ipsum",
+          category: "black",
+          price: "89.99",
+        },
+        {
+          id: 7,
+          title: "белая",
+          img: "white_4.jpeg",
+          desc: "lorem ipsum",
+          category: "white",
+          price: "89.99",
+        },
+        {
+          id: 8,
+          title: "черная",
+          img: "black_4.jpeg",
+          desc: "lorem ipsum",
+          category: "black",
+          price: "89.99",
+        },
+        {
+          id: 9,
+          title: "белая",
+          img: "white_5.jpg",
+          desc: "lorem ipsum",
+          category: "white",
+          price: "89.99",
+        },
+        {
+          id: 10,
+          title: "черная",
+          img: "black_5.jpg",
+          desc: "lorem ipsum",
+          category: "black",
+          price: "89.99",
+        },
+        {
+          id: 11,
+          title: "белая",
+          img: "white_6.jpg",
+          desc: "lorem ipsum",
+          category: "white",
+          price: "89.99",
+        },
+        {
+          id: 12,
+          title: "черная",
+          img: "black_6.jpg",
+          desc: "lorem ipsum",
+          category: "black",
+          price: "89.99",
+        },
+        {
+          id: 13,
+          title: "белая",
+          img: "white_7.jpg",
+          desc: "lorem ipsum",
+          category: "white",
+          price: "89.99",
+        },
+        {
+          id: 14,
+          title: "черная",
+          img: "black_7.jpg",
+          desc: "lorem ipsum",
+          category: "black",
+          price: "89.99",
+        },
+        {
+          id: 15,
+          title: "белая",
+          img: "white_8.jpg",
+          desc: "lorem ipsum",
+          category: "white",
+          price: "89.99",
+        },
+      ],
+      showFullItem: false,
+      fullItem: {},
+    };
+    this.state.currentItems = this.state.items;
+    this.addToOrder = this.addToOrder.bind(this);
+    this.deleteOrder = this.deleteOrder.bind(this);
+    this.chooseCategory = this.chooseCategory.bind(this);
+    this.onShowItem = this.onShowItem.bind(this);
+  }
+  render() {
+    return (
+      <div className="container">
+        <Header orders={this.state.orders} onDelete={this.deleteOrder} />
+        <div className="wrapper">
+          <Categories chooseCategory={this.chooseCategory} />
+          <Items
+            onShowItem={this.onShowItem}
+            items={this.state.currentItems}
+            onAdd={this.addToOrder}
+          />
 
-  return (
-    <>
-      <Header />
-      <div>
-        <a href="#" target="_blank">
-          <img src={headerLogo} className="logo" alt="logo" />
-        </a>
+          {this.state.showFullItem && (
+            <ShowFullItem
+              onAdd={this.addToOrder}
+              onShowItem={this.onShowItem}
+              item={this.state.fullItem}
+            />
+          )}
+        </div>
+        <Footer />
       </div>
-      <h1>Ой-Чо за бренд!!!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more Lorem ipsum, dolor sit
-        amet consectetur adipisicing elit. Iure, ab aliquid? Asperiores eaque
-        soluta distinctio provident cumque vel dolore, nulla, molestiae quam
-        expedita quod qui iusto, maxime hic similique? Quisquam!Lorem Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores
-        facere obcaecati illum excepturi, alias, corrupti debitis aliquid
-        aperiam fugiat maxime ipsa ea optio qui error aut voluptatem minima.
-        Quaerat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
-        hic neque totam. Pariatur illum esse dolor earum labore laborum saepe
-        iusto, ipsam error, nostrum deleniti obcaecati, praesentium quasi odit
-        perferendis! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Id, laudantium ab! Qui eveniet aliquam error veritatis consequuntur sit
-        sed praesentium ipsam nulla. Eveniet sapiente quasi cumque quia nemo
-        minus accusantium!
-      </p>
-      <Footer />
-    </>
-  );
+    );
+  }
+
+  onShowItem(item) {
+    this.setState({ fullItem: item });
+    this.setState({ showFullItem: !this.state.showFullItem });
+  }
+
+  chooseCategory(category) {
+    if (category === "all") {
+      this.setState({ currentItems: this.state.items });
+      return;
+    }
+
+    this.setState({
+      currentItems: this.state.items.filter((el) => el.category === category),
+    });
+  }
+
+  deleteOrder(id) {
+    this.setState({ orders: this.state.orders.filter((el) => el.id !== id) });
+  }
+
+  addToOrder(item) {
+    let isInArray = false;
+    this.state.orders.forEach((el) => {
+      if (el.id === item.id) isInArray = true;
+    });
+    if (!isInArray) this.setState({ orders: [...this.state.orders, item] });
+  }
 }
-
 export default App;
+
